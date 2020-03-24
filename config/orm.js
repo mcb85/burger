6c27ee0f8dbc0234fd6e8ec5379b86992b9dfff4
Mariana <mariana.diaz@live.com>
2020-03-24 02:10:36 -0400

    began adding orm functions to burger.js and orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,18 +1,20 @@
-var connection = require('./config/connection.js');
+var connection = require('../config/connection.js');
 
 
 var orm = {
 selectAll: function() {
-    var query = "SELECT * FROM";
+    var query = "SELECT * FROM burger_name";
     connection.query(query, function(err, result) {
         if (err) throw err;
         console.log(result);
-    })
-}
+    });
+},
+
+
 
 insertOne: function() {}
 
-updateOne: function() {}
+//updateOne: function() {}
 
 
 
--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,11 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-
+selectAll: function() {
+    orm.selectAll("burgers", function(res){
+        console.log(res);
+    })
+}
 }
 
 
